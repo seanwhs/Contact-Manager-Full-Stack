ecc25466cd1a6f27a829c2cced0ecf8a66c18ff1
seanwhs <seanwhs@gmail.com>
2024-02-19 05:29:13 +0800

    Created Backend

---
+++ contact_mgr/src/test/java/sean/contact_mgr/ContactMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package sean.contact_mgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
